c29f9461fe1ef7e7546c6a5da9d961dfa7bdf40d
kukub <kukuba2121@gmail.com>
2023-07-27 00:49:47 +0200

    dwa

--- skrypty/edge_detection.py
+++ skrypty/edge_detection.py
@@ -11,31 +11,36 @@ cv2.waitKey(0)
 #ret,thresh_img = cv2.threshold(img,127, 255, cv2.THRESH_BINARY)
 
 #Finding edges of the image
-edge_image = cv2.Canny(img,1,200)
+edge_image = cv2.Canny(img,100,300)
 #showing Edged image
 cv2.imshow("okno2",edge_image)
 cv2.waitKey(0)
 
 # Finding all the lines in an image based on given parameters
 contours, hierarchy = cv2.findContours(edge_image,
-    cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 #Reverting the original image back to BGR so we can draw in colors
 img2 = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 #parameter -1 specifies that we want to draw all the contours
 i=0
 k=0
 o=255
+x_min = []
+x_max = []
+y_min = []
+y_max = []
 for con in contours:
-    cv2.drawContours(img2, contours[i], -1, (50, k, o), 2)
-    x_min = np.min(con,axis=0)
-    x_max = np.max(con,axis=0)
-    y_min = np.min(con,axis=0)
-    y_max = np.max(con,axis=0)
-    print(x_min)
+    # cv2.drawContours(img2, contours[i], -1, (50, k, o), 2)
+    #print("i="+str(i)+str(contours[i]))
+    xmin = np.min(con,axis=0)[0][0]
+    xmax = np.max(con,axis=0)[0][0]
+    ymin = np.min(con,axis=0)[0][1]
+    ymax = np.max(con,axis=0)[0][1]
+    cv2.rectangle(img2,(xmin,ymin),(xmax,ymax),(0,255,0),1)
     i+=1
     k+=5
     o-=10
-#cv2.drawContours(img2, contours[9], -1, (50, k, o), 2)
+# print(xmin)
+cv2.drawContours(img2, contours, -1, (255, 0, 0), 1)
 cv2.imshow("okno3",img2)
 cv2.waitKey(0)
-print(len(contours))
